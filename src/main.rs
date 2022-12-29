382ade437dc679b1bbec5b2b1292787127e0ad8e
ashleyhen <hendrash@mail.gvsu.edu>
2022-12-29 18:27:24 -0500

    successful connection

--- src/main.rs
+++ src/main.rs
@@ -36,30 +36,30 @@ async fn main() {
     //     .expect("failed to get the list of peers");
 
 
-    let node_pubkey=hex::decode("02b39d3066644b9b552c98533dc3368c974dcc1408ce809057e260b132c530ab6b").expect("decoding failed");
+    let node_pubkey=hex::decode("039d1b9632c9433380a431369154ca2725d9e5153ebf2227b71e284cdbfd7a5ffe").expect("decoding failed");
     let open_channel_req=OpenChannelRequest{
         sat_per_vbyte:30,
         node_pubkey,
         local_funding_amount:1000000,
         node_pubkey_string:"".to_owned(),
-        push_sat:500000,
-        target_conf:5,
+        push_sat:750000,
+        target_conf:0,
         sat_per_byte:0,
         private:false,
         min_htlc_msat:100,
-        remote_csv_delay:50,
+        remote_csv_delay:600,
         min_confs:0,
         spend_unconfirmed:true,
         close_address:"bcrt1prnpxwf9tpjm4jll4ts72s2xscq66qxep6w9hf6sqnvwe9t4gvqasklfhyj".to_owned(),
         funding_shim:None,
-        remote_max_value_in_flight_msat:500000,
+        remote_max_value_in_flight_msat:880000000,
         remote_max_htlcs:3,
-        max_local_csv:10,
+        max_local_csv:900,
         commitment_type:0,
-        zero_conf:true,
+        zero_conf:false,
         scid_alias:false,
-        base_fee: 50,
-        fee_rate:10000,
+        base_fee: 700,
+        fee_rate:0,
         use_base_fee:false,
         use_fee_rate:false,
         remote_chan_reserve_sat:20000
